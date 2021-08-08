2f1e42615fe4c2be8948fa2018831bc0d4b93f3e
Mika Äärilä <maarila@gmail.com>
2021-08-08 11:02:41 +0300

    Front: Get data from backend

--- frontend-for-vaccine-exercise/src/App.js
+++ frontend-for-vaccine-exercise/src/App.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const App = () => {
+  const [summary, setSummary] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001').then((response) => {
+      setSummary(response.data);
+    });
+  });
+
   return (
     <>
-      <div>All is well.</div>
+      <h1>Vaccine statistics</h1>
+      <div>Expired Bottles: {summary.expired_bottles}</div>
     </>
   );
 };

9afa6d30d45853c6e8da6b4f3dfc789738c6fd73
Mika Äärilä <maarila@gmail.com>
2021-08-15 12:35:39 +0300

    Add Cypress and end-to-end tests

---
+++ cypress/integration/vaccine_app.spec.js
@@ -0,0 +1,29 @@
+describe('Vaccine statistics web app', function() {
+  it('main pages opens as expected', function() {
+    cy.visit('http://localhost:3000/api/statistics');
+    cy.contains('Vaccine statistics');
+    cy.contains('Currently showing:');
+    cy.contains(', a web dev exercise for Solita Dev Academy, 2021');
+  });
+
+  it('redirection from / functions correctly', function() {
+    cy.visit('http://localhost:3000/');
+    cy.contains('Vaccine statistics');
+    cy.contains('Currently showing:');
+    cy.contains(', a web dev exercise for Solita Dev Academy, 2021');
+  });
+
+  it('fetching first available data provides expected result', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('.datetime-picker').type('2021-01-02T13:27')
+    cy.get('.submit-button').click()
+    cy.contains('5')
+  })
+  
+  it('fetching late data provides all expired vaccines', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('.datetime-picker').type('2021-07-01T00:00')
+    cy.get('.submit-button').click()
+    cy.contains('18015')
+  })
+});
